00ad2c388f11c86c0e9fbc5587ab720f0a0d5d72
khalid ahmed <khalid.a.dev@gmail.com>
2022-12-12 10:12:07 +0300

    pyshop app committed

---
+++ products/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Product, Offer
+
+def index(request):
+    products = Product.objects.all()
+    return render(request, 'index.html', {'products': products})
+
+def new(request):
+    return HttpResponse('New Product')
